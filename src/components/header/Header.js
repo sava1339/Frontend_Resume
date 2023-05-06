edca59113b134352c11a19403cdd724ad1f706a4
Saveliy <zsava0090@gmail.com>
2023-05-06 23:07:27 +0500

    fix3

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <Link className="nav-button" to="/" ><p>Главная</p></Link>
             <Link className="nav-button" to="/experience"><p>Опыт работы</p></Link>
             <Link className="nav-button" to="/about"><p>Обо мне</p></Link>
-            <Link className="nav-button" to="/portfolio"><p>Портфолео</p></Link>
+            <Link className="nav-button" to="/portfolio"><p>Портфолио</p></Link>
             <Link className="nav-button" to="/contact"><p>Контакты</p></Link>
         </div>
     );
