f3610fad3443aa18975464ae4050194b8d35c6cd
Saveliy <zsava0090@gmail.com>
2023-05-06 23:03:27 +0500

    fix2

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,7 @@ const About = () => {
     return (
         <div className="about">
             <h2 className='about-title'>Обо мне</h2>
-            <p className='about-descr'>Привет, я Якимов Савели Junior+ developer, из Березников. Мне интересено веб-программирование и всё что связанно с ним.</p>
+            <p className='about-descr'>Привет, я Якимов Савели Junior+ developer, из Березников. Мне интересно веб-программирование и всё что связанно с ним.</p>
             <p className='about-descr'>В данный момент учусь на курсах "Разработка на JavaScript" в университете Сиинергии.</p>
             <p className='about-descr'>Готов работать с необычными проектами и с интересными людьми.</p>
         </div>
